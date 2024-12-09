3aae7ef644c85983ad7b49e37ad1ed1e5a2f7b6d
TDBrody <brody.seyranian28@student.tamdistrict.org>
2024-12-09 15:02:45 -0800

    hjvjv

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
+let score = 0;
+const scoreElement = document.getElementById('score');
+
+function subtractScore() {
+    score -= 300000;
+    scoreElement.textContent = "Score: " + score;
+
+    // Check win condition
+    if (score > 100000000) {
+        alert('You Win!');
+    }
+}
+
+setInterval(subtractScore, 1000);
+
 document.addEventListener('DOMContentLoaded', () => {
-    const image = document.getElementById('teleportImage');
-
-    image.addEventListener('click', () => {
-        const maxWidth = window.innerWidth - image.width;
-        const maxHeight = window.innerHeight - image.height;
-
-        const randomX = Math.floor(Math.random() * maxWidth);
-        const randomY = Math.floor(Math.random() * maxHeight);
-
-        image.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    const mew = document.getElementById('mew');
+    const mewAudio = document.getElementById('MewMewMeeew');
+    mew.addEventListener('click',() => {
+        addScore(0, 100000);
+        mewAudio.play();
+        mew.style.left = `${randomX}px`;
+        mew.style.top = `${randomY}px`;
+    });
+     const raikou = document.getElementById('raikou');
+     //const mewAudio = document.getElementById('MewMewMeeew');
+     mew.addEventListener('click',() => {
+         addScore(-1000000, 2690000);
+         mewAudio.play();
+         mew.style.left = `${randomX}px`;
+    -     mew.style.top = `${randomY}px`;
     });
 });
-score += Math.floor(Math.random() * 1000000) + 1;
 
-if (score > 100000000) {
-    document.getElementById('message').textContent = 'You Win!';
+function addScore(min, max) {
+        // Add a random number between 1 and 100 to the score
+        score += Math.floor(Math.random() * (max - min)) + min;
+        scoreElement.textContent = "Score: " + score;
+        const maxWidth = window.innerWidth - mew.offsetWidth;
+        const maxHeight = window.innerHeight - mew.offsetHeight;
+        const randomX = Math.floor(Math.random() * maxWidth);
+        const randomY = Math.floor(Math.random() * maxHeight);
 }
+
+var element = new Image;
+var devtoolsOpen = false;
+element.__defineGetter__("id", function () {
+    devtoolsOpen = true; // This only executes when devtools is open.
+});
+setInterval(function () {
+    devtoolsOpen = false;
+    console.log(element);
+    if (devtoolsOpen) {
+        alert("CHEATER");
+    }
+}, 100);
